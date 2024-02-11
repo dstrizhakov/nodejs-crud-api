4fb0e009e910b895966668bc3b0289bcfa3c09fa
dmirty.s <d.strizhakov@gmail.com>
2024-02-11 15:04:33 +0300

    feat: add get users and get user

--- src/Database/Database.ts
+++ src/Database/Database.ts
@@ -1,63 +1,67 @@
-import {IUser} from "../models/IUser";
-import {v4} from 'uuid';
-
+import { IUser } from '../models/IUser';
+import { v4, validate } from 'uuid';
 
 export class Database {
-    private users: IUser[] = [];
+  private users: IUser[] = [];
 
-    initUsers(count: number) {
-        for (let i = 0; i < count; i++) {
-            const user: IUser = {id: v4(), username: 'user ' + i, age: 20 + i, hobbies: []}
-            this.addUser(user);
-        }
+  initUsers(count: number) {
+    for (let i = 0; i < count; i++) {
+      const user: IUser = { id: v4(), username: 'user ' + i, age: 20 + i, hobbies: [] };
+      this.addUser(user);
     }
+  }
 
-    addUser(user: IUser) {
-        const userToAddIndex = this.users.findIndex(userDb => userDb.id === user.id);
-        if (userToAddIndex >= 0) {
-            console.error(`User id = ${user.id} is already exits in database`)
-        } else {
-            this.users.push(user)
-        }
+  addUser(user: IUser) {
+    const userToAddIndex = this.users.findIndex((userDb) => userDb.id === user.id);
+    if (userToAddIndex >= 0) {
+      console.error(`User id = ${user.id} is already exits in database`);
+    } else {
+      this.users.push(user);
     }
+  }
 
-    getUser(id: string) {
-        const userToGetIndex = this.users.findIndex(user => user.id === id);
-        if (userToGetIndex >= 0) {
-            return this.users.find(user => user.id === id);
-        } else {
-            console.error(`User id = ${id} not found in database`)
-        }
+  getUser(id: string) {
+    const userToGetIndex = this.users.findIndex((user) => user.id === id);
+    if (userToGetIndex >= 0) {
+      return this.users.find((user) => user.id === id);
+    } else {
+      console.error(`User id = ${id} not found in database`);
+      return null;
     }
+  }
 
-    getUsers() {
-        return this.users
-    }
+  getUsers() {
+    return this.users;
+  }
 
-    deleteUser(id: string) {
-        const userToDeleteIndex = this.users.findIndex(user => user.id === id);
-        if (userToDeleteIndex >= 0) {
-            this.users.filter(user => user.id !== id);
-            console.log(`User id = ${id} was deleted`);
-        } else {
-            console.error(`User id = ${id} not found in database`)
-        }
+  deleteUser(id: string) {
+    const userToDeleteIndex = this.users.findIndex((user) => user.id === id);
+    if (userToDeleteIndex >= 0) {
+      this.users.filter((user) => user.id !== id);
+      console.log(`User id = ${id} was deleted`);
+    } else {
+      console.error(`User id = ${id} not found in database`);
     }
+  }
 
-    deleteUsers() {
-        this.users = [];
-    }
+  deleteUsers() {
+    this.users = [];
+  }
 
-    updateUser(id: string, user: Omit<IUser, 'id'>) {
-        const userToUpdateIndex = this.users.findIndex(user => user.id === id);
-        if (userToUpdateIndex >= 0) {
-            this.users[userToUpdateIndex] = {
-                id,
-                ...user
-            }
-            console.log(`User id = ${id} updated`)
-        } else {
-            console.error(`User id = ${id} not found in database`)
-        }
+  updateUser(id: string, user: Omit<IUser, 'id'>) {
+    const userToUpdateIndex = this.users.findIndex((user) => user.id === id);
+    if (userToUpdateIndex >= 0) {
+      this.users[userToUpdateIndex] = {
+        id,
+        ...user,
+      };
+      console.log(`User id = ${id} updated`);
+    } else {
+      console.error(`User id = ${id} not found in database`);
     }
+  }
+
+  isUuid(uuid: string) {
+    return validate(uuid);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,54 @@
-import {Database} from "./Database/Database";
+import { config } from 'dotenv';
+import { Database } from './Database/Database';
+import * as http from 'node:http';
+import path from 'node:path';
+import { log } from 'node:console';
 
-console.log('Hello from ts!');
-let database = new Database();
+config();
+
+const PORT = process.env.PORT || 4000;
+
+const database = new Database();
 
 database.initUsers(5);
 
-console.log(database.getUsers())
+//single
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+  const Url = new URL(url as string, '/');
+  console.log(Url);
+  res.setHeader('Content-type', 'application/json');
+  try {
+    switch (true) {
+      case method === 'GET' && url === '/api/users':
+        const users = database.getUsers();
+        console.log(users);
+        res.statusCode = 200;
+        res.end(JSON.stringify(users));
+        break;
+      case method === 'GET' && url?.startsWith('/api/users/'):
+        const uuid = path.basename(url as string);
+        if (database.isUuid(uuid)) {
+          const user = database.getUser(uuid);
+          if (user) {
+            res.statusCode = 200;
+            res.end(JSON.stringify(user));
+          } else {
+            res.statusCode = 404;
+            res.end(JSON.stringify({ message: `User with id: ${uuid} doesn't exist` }));
+          }
+        } else {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ message: 'Invalid user id' }));
+        }
+        break;
+      default:
+        res.statusCode = 404;
+        res.end(JSON.stringify({ message: 'Request to non-existing endpoint' }));
+    }
+  } catch (error: unknown) {}
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
